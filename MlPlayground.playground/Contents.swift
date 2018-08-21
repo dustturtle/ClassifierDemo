678db4005cb627a4578bc5d35059c1235982f1fe
Zhenwei Guan <openglnewbee@163.com>
2018-08-21 20:17:01 +0800

    两种写法的createML使用(playgound)，数据准备->模型构建->模型训练->模型评估->导出

---
+++ MlPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+// first: the simplest UI version. Need user interactive, with MLImageClassifierBuilder.
+
+//import CreateMLUI
+//let builder = MLImageClassifierBuilder()
+//builder.showInLiveView()
+
+
+// second: auto with codes version. with MLImageClassifier and logic codes.
+import CreateML
+import Foundation
+
+let trainDir = URL(fileURLWithPath: "/Users/zhenweiguan/Desktop/MLPlayground/FruitImages/TrainingData")
+let testDir = URL(fileURLWithPath: "/Users/zhenweiguan/Desktop/MLPlayground/FruitImages/TestingData")
+
+let model = try MLImageClassifier(trainingData: .labeledDirectories(at: trainDir))
+
+let eval = model.evaluation(on: .labeledDirectories(at: testDir))
+
+try model.write(to: URL(fileURLWithPath: "/Users/zhenweiguan/Desktop/MLPlayground/FruitImages/FruitClassifier"))
